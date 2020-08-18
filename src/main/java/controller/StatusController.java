f33d5c5978579320c3682ba1dd2507c92423bf84
kongweicao <weicao8@126.com>
2020-08-18 10:41:38 +0800

    add

---
+++ src/main/java/controller/StatusController.java
@@ -0,0 +1,19 @@
+package controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 〈功能概述〉<br>
+ *
+ * @author: Mike
+ * @date: 2020/8/18 10:36
+ * @DESCRIPTION:
+ */
+@RestController
+public class StatusController {
+    @GetMapping("Status")
+    public String getStatus(){
+        return "jenkins build success!";
+    }
+}
