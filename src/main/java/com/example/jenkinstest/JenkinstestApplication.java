9045a35b0b45801e6596778bea4e7e2f75517acb
kongweicao <weicao8@126.com>
2020-08-19 10:38:43 +0800

    jar

--- src/main/java/com/example/jenkinstest/JenkinstestApplication.java
+++ src/main/java/com/example/jenkinstest/JenkinstestApplication.java
@@ -1,12 +1,15 @@
 package com.example.jenkinstest;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class JenkinstestApplication {
 
     public static void main(String[] args) {
+        log.info("=====================jenkins-test启动===========================");
         SpringApplication.run(JenkinstestApplication.class, args);
     }
 
